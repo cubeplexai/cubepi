070ddb7288831e13a78e6eb868ba9d2313c5c0e6
xf gong <gongxiangfeng@sensedeal.ai>
2026-05-20 17:06:46 +0000

    feat(images): images subsystem types

---
+++ cubepi/providers/images/__init__.py
@@ -0,0 +1,15 @@
+from cubepi.providers.images.types import (
+    AssistantImages,
+    ImagesContext,
+    ImagesModel,
+    ImagesQuality,
+    ImagesSize,
+)
+
+__all__ = [
+    "AssistantImages",
+    "ImagesContext",
+    "ImagesModel",
+    "ImagesQuality",
+    "ImagesSize",
+]
